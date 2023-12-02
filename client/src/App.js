7460b4de1e06e47eec943dc35f30a49c3dba5838
Duc Huy <huyv3451@gmail.com>
2023-12-02 19:22:49 +0700

    message

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import Axios from "axios"
 
-function App() {
+
+const App=() => {
+  const [data, setData]=useState("")
+
+  const getData=async()=> {
+    const response=await Axios.get("http://localhost:3001/getData")
+    setData(response.data)
+  }
+
+  useEffect(()=> {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      Hello World
+      {data}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
 const express = require('express')
-const sql = require("msnodesqlv8")
+const cors = require('cors')
+// const sql = require("msnodesqlv8")
+
+
 const app = express()
+app.use(cors())
 
-const connectionString = "server=.;Database=QLNK;Trusted_Connection=Yes;Driver={SQL Server}";
-const query = "SELECT * FROM dbo.BACSI WHERE MA_BS = 000003";
+// const connectionString = "server=.;Database=QLNK;Trusted_Connection=Yes;Driver={SQL Server}";
+// const query = "SELECT * FROM dbo.BACSI WHERE MA_BS = 000003";
 
-sql.queryRaw(connectionString, query, (err, result) => {
-  if (err) {
-      console.error("Error executing query:", err);
-      return;
-  }
-  const rows = result && result.rows ? result.rows : [];
-  console.log("Result:", rows);
-});
+// sql.queryRaw(connectionString, query, (err, result) => {
+//   if (err) {
+//       console.error("Error executing query:", err);
+//       return;
+//   }
+//   const rows = result && result.rows ? result.rows : [];
+//   console.log("Result:", rows);
+// });
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/getData', function (req, res) {
+  res.send('Successful connect to backend ')
 })
 
 app.listen(3001, () => {
